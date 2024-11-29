92153f64c128a04459104e9f5c8b9ccf75313e5d
osamabinzahid <osamabinzahid@gmail.com>
2024-11-30 00:39:58 +0500

    Chai or React

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+  const username = "osama bin zahid"
+  return (
+    <>
+    <Chai/>
+    <h1>Testing {username}</h1>
+    <h2>Testing 2</h2>
+    </>
+  )
+}
+
+export default App
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+ 
+  //let counter = 15
+
+  const addValue = () => {
+  // console.log("clicked", counter)
+   //counter = counter + 1
+   setCounter(counter + 1)
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+        <h1>Counter Web Application</h1>
+        <h2>Conuter value: {counter}</h2>
+
+        <button onClick={addValue}>Add Value</button>
+        <br />
+        <button onClick={removeValue}>Remove Buttom</button>
+    </>
+  )
+}
+
+export default App
