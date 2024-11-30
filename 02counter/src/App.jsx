0671e00dd9122e11e3d74c5d0c933d2b94ad69d0
osamabinzahid <osamabinzahid@gmail.com>
2024-12-01 01:07:48 +0500

    bgchangerApp

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -12,6 +12,9 @@ function App() {
   const addValue = () => {
   // console.log("clicked", counter)
    //counter = counter + 1
+   //setCounter((prevCounter) => prevCounter +1)
+   //setCounter((prevCounter) => prevCounter +1)
+   //setCounter((prevCounter) => prevCounter +1)
    setCounter(counter + 1)
   }
 
